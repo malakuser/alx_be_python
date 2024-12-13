5395206b797d6b97cb1b0a945e42c17873d7fbfb
malakuser <malakdev024@gmail.com>
2024-12-13 20:00:26 +0100

    first commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+result = number1 + number2
+result1 = number1 - number2
+result2 = number1 * number2
+
+print("Addition of", number1,"and", number2,"is", result)
+print("Substraction of", number1,"and", number2,"is", result1)
+print("Multiplication of", number1,"and", number2,"is", result2)
